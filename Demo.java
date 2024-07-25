6735df63516bd3e59beb02da0b68a485b26a4ac3
Md Fariyad <mdfariyad177@gmail.com>
2024-07-25 22:14:39 +0530

    Array of objects

--- Demo.java
+++ Demo.java
@@ -1,18 +1,28 @@
-//  2D ARRAY
+//  object of array
 
 import java.util.*;
+
+class Human {
+  public int x = 10;
+  public int y = 887;
+
+}
 
 public class Demo {
 
   public static void main(String[] args) {
 
-    int[][] nums = { { 20, 30, 40, 50 },
-        { 4, 5, 3, 3, 6 } };
+    Human h1 = new Human();
+    Human h2 = new Human();
+    Human h3 = new Human();
+    Human[] arr = new Human[3];
+    arr[0] = h1;
+    arr[1] = h2;
+    arr[2] = h3;
 
-    for (int[] ele : nums) {
-      for (int value : ele)
-        System.out.print(value);
-      System.out.println();
+    for (Human value : arr) {
+      System.out.println(value.x);
+      System.out.println(value.y);
     }
 
   }
